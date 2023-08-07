8c57207458cdf99176135f4c81cef5f8790e80d5
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 15:27:09 +0200

    Fix test file

--- infer_neural_style_transfer_test.py
+++ infer_neural_style_transfer_test.py
@@ -2,7 +2,6 @@ import logging
 import cv2
 from ikomia.utils.tests import run_for_test
 from infer_neural_style_transfer.utils import model_zoo
-from ikomia.core import task
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +14,7 @@ def test(t, data_dict):
     input_img_0.set_image(img)
     for method, models in model_zoo.items():
         for model_name, _ in models.items():
-            params = t.get_param_object()
+            params = t.get_parameters()
             params["model_name"] = model_name
             params["method"] = method
             t.set_parameters(params)
