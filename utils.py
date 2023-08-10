962b9e8cc631cb985ad1e52b2fe039e83e9b3f1c
allankouidri <allan.kouidri@gmail.com>
2023-08-10 14:38:43 +0200

    Update model weights link

--- utils.py
+++ utils.py
@@ -4,41 +4,41 @@ import os
 model_zoo = {
     "eccv16": {
         "the_wave": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/the_wave.t7",
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/the_wave.t7",
             "img": "the_wave.jpg"},
         "la_muse": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/la_muse.t7",
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/la_muse.t7",
             "img": "la_muse.jpg"},
         "starry_night": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/starry_night.t7",
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/starry_night.t7",
             "img": "starry_night.jpg"},
         "composition_vii": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/composition_vii.t7",
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/eccv16/composition_vii.t7",
             "img": "composition_vii.jpg"},
 
     },
     "instance_norm": {"candy": {
-        "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models"
+        "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models"
                  "/instance_norm/candy.t7",
         "img": "candy.jpg"},
         "mosaic": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
                      "/mosaic.t7",
             "img": "mosaic.jpg"},
         "the_scream": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
                      "/the_scream.t7",
             "img": "the_scream.jpg"},
         "udnie": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
                      "/udnie.t7",
             "img": "udnie.jpg"},
         "feathers": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
                      "/feathers.t7",
             "img": "feathers.jpg"},
         "la_muse": {
-            "model": "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
+            "model": "https://cs.stanford.edu/people/jcjohns/fast-neural-style/models/instance_norm"
                      "/la_muse.t7",
             "img": "la_muse.jpg"}
     }
@@ -47,9 +47,12 @@ model_zoo = {
 
 def download_model(method, name, models_folder):
     URL = model_zoo[method][name]["model"]
-    response = requests.get(URL)
-    model_folder = os.path.join(models_folder, method)
-    os.makedirs(model_folder, exist_ok=True)
 
-    with open(os.path.join(model_folder, f"{name}.t7"), "wb") as f:
-        f.write(response.content)
+# Download the dataset
+    model_folder = os.path.join(models_folder, method)
+    print(URL)
+    response = requests.get(URL, stream=True)
+    with open(os.path.join(model_folder, f"{name}.t7"), "wb") as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
