943e6399298bc8a422a7412b530bd6d4d69dff5d
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2025-12-02 11:07:06 +0100

    chore: update version

--- infer_neural_style_transfer_process.py
+++ infer_neural_style_transfer_process.py
@@ -172,7 +172,7 @@ class NeuralStyleTransferFactory(dataprocess.CTaskFactory):
         self.info.short_description = "Neural network method to paint given image in the style of the reference image."
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Art"
-        self.info.version = "1.2.0"
+        self.info.version = "1.2.1"
         self.info.icon_path = "icon/icon.png"
         self.info.authors = "Justin Johnson, Alexandre Alahi, Li Fei-Fei"
         self.info.article = "Perceptual Losses for Real-Time Style Transfer and Super-Resolution."
